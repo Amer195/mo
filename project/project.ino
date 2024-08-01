48276cb6f596e783e6c3f79e534dd3548c11e8d0
Amer195 <163016879+Amer195@users.noreply.github.com>
2024-08-01 14:52:43 +0200

    a7a

---
+++ project/project.ino
@@ -0,0 +1,83 @@
+/*
+   -- New project --
+   
+   This source code of graphical user interface 
+   has been generated automatically by RemoteXY editor.
+   To compile this code using RemoteXY library 3.1.11 or later version 
+   download by link http://remotexy.com/en/library/
+   To connect using RemoteXY mobile app by link http://remotexy.com/en/download/                   
+     - for ANDROID 4.11.4 or later version;
+     - for iOS 1.9.1 or later version;
+    
+   This source code is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.    
+*/
+
+//////////////////////////////////////////////
+//        RemoteXY include library          //
+//////////////////////////////////////////////
+
+// RemoteXY select connection mode and include library 
+#define REMOTEXY_MODE__ESP8266_SOFTSERIAL_POINT
+#include <SoftwareSerial.h>
+
+#include <RemoteXY.h>
+
+// RemoteXY connection settings 
+#define REMOTEXY_SERIAL_RX 2
+#define REMOTEXY_SERIAL_TX 3
+#define REMOTEXY_SERIAL_SPEED 9600
+#define REMOTEXY_WIFI_SSID "RemoteXY"
+#define REMOTEXY_WIFI_PASSWORD "12345678"
+#define REMOTEXY_SERVER_PORT 6377
+
+
+// RemoteXY configurate  
+#pragma pack(push, 1)
+uint8_t RemoteXY_CONF[] =   // 69 bytes
+  { 255,2,0,0,0,62,0,16,31,1,10,48,17,4,28,28,4,26,31,79,
+  78,0,31,79,70,70,0,10,48,16,53,29,29,4,26,31,79,78,0,31,
+  79,70,70,0,129,0,22,35,18,6,8,66,117,122,122,101,114,0,129,0,
+  26,85,10,6,242,76,69,68,0 };
+  
+// this structure defines all the variables and events of your control interface 
+struct {
+
+    // input variables
+  uint8_t pushSwitch_1; // =1 if state is ON, else =0 
+  uint8_t pushSwitch_2; // =1 if state is ON, else =0 
+
+    // other variable
+  uint8_t connect_flag;  // =1 if wire connected, else =0 
+
+} RemoteXY;
+#pragma pack(pop)
+
+/////////////////////////////////////////////
+//           END RemoteXY include          //
+/////////////////////////////////////////////
+
+
+
+void setup() 
+{
+  RemoteXY_Init (); 
+  
+  
+  // TODO you setup code
+  
+}
+
+void loop() 
+{ 
+  RemoteXY_Handler ();
+  
+  
+  // TODO you loop code
+  // use the RemoteXY structure for data transfer
+  // do not call delay(), use instead RemoteXY_delay() 
+
+
+}
